47ed922acda68b4a9bf443d0eb83bb60feb6b845
Jonathan Way <Jonathan.Way@iam.local>
2018-10-02 15:02:02 -0500

    Jump Fix
    - made it so that the player character has a detector for if its on the ground
    - made it so that the jump only works if the player character is on the ground and pressing the jump button

--- Assets/Scripts/Player_Character.cs
+++ Assets/Scripts/Player_Character.cs
@@ -8,29 +8,62 @@ public class Player_Character : MonoBehaviour {
     private float accelearationForce = 5;
 
     [SerializeField]
+    private float jumpForce = 5;
+
+    [SerializeField]
     private float maxSpeed = 10;
 
     [SerializeField]
     private Rigidbody2D rb2d;
 
-    private float horizontalInput;
+    [SerializeField]
+    private Collider2D groundDetectTrigger;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
+
+
+
+    private float horizontalInput;
+    private bool isOnGround;
+    private Collider2D[] groundHitDetectionResults = new Collider2D[16];
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        UpdateIsOnGround();
+        UpdateHorizontalInput();
+        Jump();
     }
 
+    private void UpdateIsOnGround()
+    {
+        isOnGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
+        //Debug.Log("IsOnGround?: " + isOnGround);
+    }
     private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         rb2d.AddForce(Vector2.right * horizontalInput * accelearationForce);
         Vector2 clampedVelocity = rb2d.velocity;
         clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
         rb2d.velocity = clampedVelocity;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void UpdateHorizontalInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 }
